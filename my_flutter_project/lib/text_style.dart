d7681707c59250442d42abd620c050d6996f4c73
Muhammad Talha Khan <utubefortk@gmail.com>
2026-02-24 03:28:44 +0500

    text styles

---
+++ my_flutter_project/lib/text_style.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // for data change in widget we use stateless widget
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue)),
+      home: const MyHomePage(title: 'Flutter Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Theme.of(context).colorScheme.inversePrimary, title: Text(widget.title)),
+      body: Center(
+        child: Text(
+          "This is My STYLE",
+          style: TextStyle(fontSize: 25, color: Colors.deepOrange, fontWeight: FontWeight.w900, backgroundColor: Colors.lightGreenAccent),
+        ),
+      ),
+    );
+  }
+}
