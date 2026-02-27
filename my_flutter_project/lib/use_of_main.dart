e72cdd282a085413d694a4058f532be11f8a0b09
Muhammad Talha Khan <utubefortk@gmail.com>
2026-02-28 03:46:59 +0500

    MyHomePageState defined

--- my_flutter_project/lib/use_of_main.dart
+++ my_flutter_project/lib/use_of_main.dart
@@ -36,21 +36,13 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+// this is the state of our home page, this is where we will make changes to our home page, this is where we will change the UI of our home page.
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
