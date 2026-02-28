a8083cbbe4dd2b2b88718b7a6a9aa2133c10b0e8
Muhammad Talha Khan <utubefortk@gmail.com>
2026-03-01 03:04:33 +0500

    main method return type defined

---
+++ my_flutter_project/lib/build_app_from_scratch.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+// void means that this function doesn't return anything, it is just running the app and it doesn't return any value.
+/* we can also remove the void and it will work fine (as it is not returning anything), 
+/ but it is a good practice to keep it as it is, because it makes our code 
+/ more readable and understandable. */
+void main() {
+  runApp(const MyApp());
+}
