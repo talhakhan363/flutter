8901bcec6a58f11e77d820c9a3954c9d344e9923
Muhammad Talha Khan <utubefortk@gmail.com>
2026-03-01 03:41:32 +0500

    _TalhasHomePageState defined

--- my_flutter_project/lib/build_app_from_scratch.dart
+++ my_flutter_project/lib/build_app_from_scratch.dart
@@ -26,6 +26,9 @@ class TalhasApp extends StatelessWidget {
 class TalhasHomePage extends StatefulWidget {
   @override
   State<TalhasHomePage> createState() => _TalhasHomePageState();
+  /* we create the state of our home page, because we want to 
+  change the UI of our home page, we want to make it dynamic, 
+  we want to make it interactive, so we need to create the state of our home page. */
 }
 
 class _TalhasHomePageState extends State<TalhasHomePage> {
