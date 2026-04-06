f4286b46988a70c604edef5dbcf3599fbfcc74be
Muhammad Talha Khan <utubefortk@gmail.com>
2026-04-06 23:27:09 +0500

    Create list_tile_widget.dart
    initial code applied

---
+++ my_flutter_project/lib/list_tile_widget.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue)),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    /* --> we use this array for dynamic inputs from any database or api.
+    better than listView bcz in that we had to input in the widget directly. */
+    var arrNames = ["Talha", "Younus", "Ali", "Furqan", "Abdullah", "Saim", "Ahmed"];
+
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Theme.of(context).colorScheme.inversePrimary, title: Text(widget.title)),
+      body: ListView.separated(
+        itemBuilder: (context, index) {
+          // array has been taken above.
+          return ListTile(
+            leading: Text('${index + 1}', style: TextStyle(fontSize: 17)),
+            title: Text(arrNames[index], style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold)),
+            subtitle: Text("Person Contact", style: TextStyle(fontSize: 18)),
+            trailing: Icon(Icons.account_circle),
+          );
+        },
+        itemCount: arrNames.length,
+        separatorBuilder: (context, index) {
+          return Divider(height: 8, thickness: 3);
+        },
+      ),
+    );
+  }
+}
