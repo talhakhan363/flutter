260e476c6bec010d6c8c95ae0046e9f913d4059a
Muhammad Talha Khan <utubefortk@gmail.com>
2026-04-06 23:55:25 +0500

    analyzing UI using ListTile

--- my_flutter_project/lib/list_tile_widget.dart
+++ my_flutter_project/lib/list_tile_widget.dart
@@ -33,7 +33,27 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     /* --> we use this array for dynamic inputs from any database or api.
     better than listView bcz in that we had to input in the widget directly. */
-    var arrNames = ["Talha", "Younus", "Ali", "Furqan", "Abdullah", "Saim", "Ahmed"];
+    var arrNames = [
+      "Talha",
+      "Younus",
+      "Ali",
+      "Furqan",
+      "Abdullah",
+      "Saim",
+      "Ahmed",
+      "Akram",
+      "Shahid",
+      "Shahbaz",
+      "Shoaib",
+      "Asif",
+      "Aslam",
+      "Ahsan",
+      "Adeel",
+      "Adnan",
+      "Aamir",
+      "Aftab",
+      "Aqib",
+    ];
 
     return Scaffold(
       appBar: AppBar(backgroundColor: Theme.of(context).colorScheme.inversePrimary, title: Text(widget.title)),
@@ -42,13 +62,16 @@ class _MyHomePageState extends State<MyHomePage> {
           // array has been taken above.
           return ListTile(
             // leading is the first element of the list tile.
-            leading: Text('${index + 1}', style: TextStyle(fontSize: 17)),
+            leading: Text(
+              '${index + 1}',
+              style: TextStyle(fontSize: 11, color: Colors.blue, fontWeight: FontWeight.bold),
+            ),
             // title is the second element of the list tile.
-            title: Text(arrNames[index], style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold)),
+            title: Text(arrNames[index], style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
             // subtitle is the third element of the list tile.
-            subtitle: Text("Person Contact", style: TextStyle(fontSize: 18)),
+            subtitle: Text("Person Contact", style: TextStyle(fontSize: 11)),
             // trailing is the last element of the list tile.
-            trailing: Icon(Icons.account_circle),
+            trailing: Icon(Icons.account_circle, size: 23, color: Colors.blue),
           );
         },
         itemCount: arrNames.length,
