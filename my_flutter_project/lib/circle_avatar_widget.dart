5ea5abe5bc5961b0042ed6dc76ab0d198b1dae3f
Muhammad Talha Khan <utubefortk@gmail.com>
2026-04-07 13:17:02 +0500

    height adjusted

--- my_flutter_project/lib/circle_avatar_widget.dart
+++ my_flutter_project/lib/circle_avatar_widget.dart
@@ -35,47 +35,62 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(backgroundColor: Theme.of(context).colorScheme.inversePrimary, title: Text(widget.title)),
       body: Column(
         children: [
-          Container(
-            color: Colors.amberAccent,
-            padding: EdgeInsets.all(8),
-            width: 120,
-            height: 120,
-            child: CircleAvatar(backgroundImage: AssetImage("assets/images/flutter.jpg")),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              color: Colors.amberAccent,
+              padding: EdgeInsets.all(8),
+              width: 100,
+              height: 100,
+              child: CircleAvatar(backgroundImage: AssetImage("assets/images/flutter.jpg")),
+            ),
           ),
-          Container(
-            color: Colors.amberAccent,
-            padding: EdgeInsets.all(8),
-            width: 120,
-            height: 120,
-            child: CircleAvatar(backgroundImage: AssetImage("assets/images/flutter1.png"), backgroundColor: Colors.cyanAccent),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              color: Colors.amberAccent,
+              padding: EdgeInsets.all(8),
+              width: 100,
+              height: 100,
+              child: CircleAvatar(backgroundImage: AssetImage("assets/images/flutter1.png"), backgroundColor: Colors.cyanAccent),
+            ),
           ),
-          Container(
-            color: Colors.amberAccent,
-            padding: EdgeInsets.all(8),
-            width: 120,
-            height: 120,
-            child: CircleAvatar(
-              backgroundImage: AssetImage("assets/images/person1.jpg"),
-              backgroundColor: Colors.grey,
-              child: Text(
-                "PROFESSOR",
-                style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              color: Colors.amberAccent,
+              padding: EdgeInsets.all(8),
+              width: 100,
+              height: 100,
+              child: CircleAvatar(
+                backgroundImage: AssetImage("assets/images/person1.jpg"),
+                backgroundColor: Colors.grey,
+                child: Text(
+                  "PROFESSOR",
+                  style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+                ),
               ),
             ),
           ),
-          Container(
-            color: Colors.amberAccent,
-            padding: EdgeInsets.all(8),
-            width: 120,
-            height: 120,
-            child: CircleAvatar(backgroundImage: AssetImage("assets/images/flutter2.png"), backgroundColor: Colors.blueAccent.shade100),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              color: Colors.amberAccent,
+              padding: EdgeInsets.all(8),
+              width: 100,
+              height: 100,
+              child: CircleAvatar(backgroundImage: AssetImage("assets/images/flutter2.png"), backgroundColor: Colors.blueAccent.shade100),
+            ),
           ),
-          Container(
-            color: Colors.amberAccent,
-            padding: EdgeInsets.all(8),
-            width: 120,
-            height: 120,
-            child: CircleAvatar(backgroundImage: AssetImage("assets/images/person.png"), backgroundColor: Colors.deepOrangeAccent),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              color: Colors.amberAccent,
+              padding: EdgeInsets.all(8),
+              width: 100,
+              height: 100,
+              child: CircleAvatar(backgroundImage: AssetImage("assets/images/person.png"), backgroundColor: Colors.deepOrangeAccent),
+            ),
           ),
         ],
       ),
