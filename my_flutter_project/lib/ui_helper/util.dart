fd63070c0e521e60d2ec7f5e39aa64c85d5edacc
Muhammad Talha Khan <utubefortk@gmail.com>
2026-04-07 14:29:15 +0500

    comments added to get better understandings

--- my_flutter_project/lib/ui_helper/util.dart
+++ my_flutter_project/lib/ui_helper/util.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 
 // --> function to return text with hard coded functionalities.
+
 TextStyle myTextStyle22() {
   return const TextStyle(fontSize: 22, fontWeight: FontWeight.bold);
 }
+/* [myTextStyle22] function will always return text with font 
+ size 22 and font weight bold, we cannot change it because there
+ is no parameter in it. but in [myTextStyleCustom] function we 
+ can change text color and font weight by passing different 
+ values for these parameters while calling this function. */
 
 // --> custom like function.
 TextStyle myTextStyleCustom({Color textColor = Colors.black, FontWeight fontWeight = FontWeight.normal}) {
   return TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: textColor);
+  /* by setting default parameters we made this funtion to 
+  automatically set text color to black and font weight to 
+  normal if we do not pass any value for these parameters while
+  calling this function. */
 }
