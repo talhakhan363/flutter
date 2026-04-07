d377bc4c8698147449dd3e68961b6c033a34ad60
Muhammad Talha Khan <utubefortk@gmail.com>
2026-04-07 12:53:52 +0500

    Create circle_avatar_widget.dart
    initial code applied

---
+++ my_flutter_project/lib/circle_avatar_widget.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue)),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Theme.of(context).colorScheme.inversePrimary, title: Text(widget.title)),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(8),
+            width: 120,
+            height: 120,
+            child: CircleAvatar(backgroundImage: AssetImage("assets/images/flutter.jpg")),
+          ),
+          Container(
+            padding: EdgeInsets.all(8),
+            width: 120,
+            height: 120,
+            child: CircleAvatar(backgroundImage: AssetImage("assets/images/flutter1.png"), backgroundColor: Colors.cyanAccent),
+          ),
+          Container(
+            padding: EdgeInsets.all(8),
+            width: 120,
+            height: 120,
+            child: CircleAvatar(
+              backgroundImage: AssetImage("assets/images/person1.jpg"),
+              backgroundColor: Colors.grey,
+              child: Text(
+                "PROFESSOR",
+                style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(8),
+            width: 120,
+            height: 120,
+            child: CircleAvatar(backgroundImage: AssetImage("assets/images/flutter2.png"), backgroundColor: Colors.blueAccent.shade100),
+          ),
+          Container(
+            padding: EdgeInsets.all(8),
+            width: 120,
+            height: 120,
+            child: CircleAvatar(backgroundImage: AssetImage("assets/images/person.png"), backgroundColor: Colors.deepOrangeAccent),
+          ),
+        ],
+      ),
+    );
+  }
+}
