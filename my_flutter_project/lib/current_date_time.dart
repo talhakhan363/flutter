3d529094e46a454793da7f6d726f74c784e8bd18
Muhammad Talha Khan <utubefortk@gmail.com>
2026-04-17 00:24:45 +0500

    Update current_date_time.dart

--- my_flutter_project/lib/current_date_time.dart
+++ my_flutter_project/lib/current_date_time.dart
@@ -42,15 +42,28 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              //Text("Current Date Time:\n $time", style: TextStyle(fontSize: 25),),
-              Text("Current Date Time:\n ${time.hour}", style: TextStyle(fontSize: 25)),
+              // WE SET CURRENT TIME & DATE HERE.
+              Text(
+                // --> time.
+                // Current time in 24 hours format is taken through DateTime.now() method.
+                "Current Time: ${time.hour.toString().padLeft(2, '0')}:${time.minute.toString().padLeft(2, '0')}:${time.second.toString().padLeft(2, '0')}",
+                style: TextStyle(fontSize: 19),
+              ),
+
+              // if we want to make 0:9 too 00:09 then we can use padLeft() method.
+              Text(
+                // --> date.
+                "Current Date: ${time.day.toString().padLeft(2, '0')}-${time.month.toString().padLeft(2, '0')}-${time.year}",
+                style: TextStyle(fontSize: 19),
+              ),
               ElevatedButton(
                 onPressed: () {
                   setState(() {
-                    // --> recalls build function to set date & time again.
+                    // recalls build function to set date & time again.
+                    // uses when we want to update something on screen.
                   });
                 },
-                child: Text("Current Date & Time", style: TextStyle(fontSize: 20)),
+                child: Text("Current Date & Time", style: TextStyle(fontSize: 17)),
               ),
             ],
           ),
