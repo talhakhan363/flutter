232fa3b45d71ad788755c172627089c94b84f6be
Muhammad Talha Khan <utubefortk@gmail.com>
2026-04-16 13:44:46 +0500

    Create card_widget.dart
    initial code applied

---
+++ my_flutter_project/lib/card_widget.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue)),
+      home: const MyHomePage(title: 'Card Widget'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Theme.of(context).colorScheme.inversePrimary, title: Text(widget.title)),
+      body: Center(
+        child: Card(
+          elevation: 10,
+          color: Colors.blue.shade900,
+          shadowColor: Colors.blue.shade700,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "Hello Talha",
+              style: TextStyle(fontSize: 31, fontWeight: FontWeight.bold, color: Colors.yellowAccent),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
