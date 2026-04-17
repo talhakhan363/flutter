727a3a5c5fb2cce4eeea89522426053f15d1822b
Muhammad Talha Khan <utubefortk@gmail.com>
2026-04-17 12:23:08 +0500

    Update date_picker.dart

--- my_flutter_project/lib/date_picker.dart
+++ my_flutter_project/lib/date_picker.dart
@@ -43,7 +43,8 @@ class _MyHomePageState extends State<MyHomePage> {
                   // this is DatePicker, which allows the user to select a date from a calendar view. It takes in the context, the first date that can be selected, and the last date that can be selected as parameters.
                   // This is an asynchronous function because we need to wait for the user to select a date before we can proceed.
                   () async {
-                    DateTime? datePicked = await showDatePicker(context: context, firstDate: DateTime(2021), lastDate: DateTime(2025));
+                    DateTime? datePicked = await showDatePicker(context: context, firstDate: DateTime(2021), lastDate: DateTime(2026));
+                    // If the user selects a date, the datePicked variable will be assigned the selected date. If the user cancels the date picker, the datePicked variable will be null.
                     if (datePicked != null) {
                       print(
                         "Date selected: "
@@ -52,12 +53,15 @@ class _MyHomePageState extends State<MyHomePage> {
                         "${datePicked.year}",
                       );
                     }
+                    // we used this IF statement to check if the user selected a date or not. If the user selected a date, we print the selected date in the console. If the user canceled the date picker, we do nothing.
                   },
-              child: Text("Show", style: TextStyle(fontSize: 20)),
+              child: Text("Show", style: TextStyle(fontSize: 13)),
             ),
-            Text("Select time here", style: TextStyle(fontSize: 25)),
+            Text("Select time here", style: TextStyle(fontSize: 19)),
             ElevatedButton(
               onPressed: () async {
+                // this is TimePicker, which allows the user to select a time from a clock view. It takes in the context and the initial time as parameters.
+                // its asynchronous function because we need to wait for the user to select a time before we can proceed.
                 TimeOfDay? timePicked = await showTimePicker(context: context, initialTime: TimeOfDay.now());
                 if (timePicked != null) {
                   print(
@@ -66,8 +70,9 @@ class _MyHomePageState extends State<MyHomePage> {
                     "${timePicked.minute}",
                   );
                 }
+                // we used this IF statement to check if the user selected a time or not. If the user selected a time, we print the selected time in the console. If the user canceled the time picker, we do nothing.
               },
-              child: Text("Show", style: TextStyle(fontSize: 20)),
+              child: Text("Show", style: TextStyle(fontSize: 13)),
             ),
           ],
         ),
