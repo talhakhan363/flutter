28637591ae4babb640c556f8e168639217851b85
Muhammad Talha Khan <utubefortk@gmail.com>
2026-04-18 14:18:11 +0500

    Update rounded_button.dart

--- my_flutter_project/lib/widgets/rounded_button.dart
+++ my_flutter_project/lib/widgets/rounded_button.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 
+// --> creating a class for custom button.
 class RoundedButton extends StatelessWidget {
-  final String btnName;
-  final Icon? btnIcon;
-  final Color? btnBgColor;
-  final TextStyle? btnTextStyle;
-  final VoidCallback? callback;
-  /* --> voidCallBack doesn't take any argument nor return any value, it just
-  run the code provided to it, hence the run-able code will be in the function
-  we are using actually... */
+  final String btnName; // --> button name
+  final Icon? btnIcon; // --> button icon (can be null).
+  final Color? btnBgColor; // --> button background color (can be null).
+  final TextStyle? btnTextStyle; // --> button text style (can be null).
+  final VoidCallback? callback; // --> button callback function (can be null).
+
+  /* callback function is a function that is passed as an argument to another function.
+  it doesn't have any parameters and it doesn't return any value.
+  mainly used to handle events or to perform some action after a certain task is completed. */
 
   const RoundedButton({
     super.key,
@@ -25,7 +27,7 @@ class RoundedButton extends StatelessWidget {
     return ElevatedButton(
       onPressed: () {
         callback!();
-        /* --? callback gives error due to probability of null value, so callback!
+        /* --? callback gives error due to probability of null value, so [callback!]
       means we will not enter it null */
       },
       style: ElevatedButton.styleFrom(
@@ -34,6 +36,7 @@ class RoundedButton extends StatelessWidget {
         shadowColor: btnBgColor,
       ),
       child: btnIcon != null
+          // using row to show both icon and text in button.
           ? Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
