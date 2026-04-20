9f1a185107a9187505b3afee09ddcf094e5d9759
Muhammad Talha Khan <utubefortk@gmail.com>
2026-04-20 09:33:51 +0500

    Update icons_widget.dart

---
+++ my_flutter_project/lib/icons_widget.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue)),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade500,
+        titleTextStyle: TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
+        title: Text(widget.title),
+      ),
+
+      body:
+          // Icon widget is used to display an icon.
+          // It is a basic widget that is used to display an icon from the Material Icons font.
+          Icon(Icons.flag, size: 21, color: Colors.pinkAccent.shade700),
+    );
+  }
+}
