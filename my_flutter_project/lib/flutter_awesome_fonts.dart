e7a6d7a3e29ce78da4821638fd15ef210dadbc8d
Muhammad Talha Khan <utubefortk@gmail.com>
2026-04-20 10:32:25 +0500

    Update flutter_awesome_fonts.dart

--- my_flutter_project/lib/flutter_awesome_fonts.dart
+++ my_flutter_project/lib/flutter_awesome_fonts.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-// --> above package is used for font_awesome_flutter....
+// above package is used for font_awesome_flutter....
 
 void main() {
   runApp(const MyApp());
@@ -40,15 +40,26 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
 
-      body: Column(
-        children: [
-          Icon(Icons.account_circle, size: 71, color: Colors.pinkAccent),
-          Icon(Icons.accessibility, size: 71, color: Colors.indigo),
-          FaIcon(FontAwesomeIcons.amazon, size: 71, color: Colors.green.shade300),
-          FaIcon(FontAwesomeIcons.google, size: 71, color: Colors.teal.shade700),
-          FaIcon(FontAwesomeIcons.microsoft, size: 71, color: Colors.redAccent),
-          FaIcon(FontAwesomeIcons.school, size: 71, color: Colors.grey),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // here we are using flutter's default icons and font awesome icons in the same column....
+
+            // normal fonts icon:
+            Icon(Icons.account_circle, size: 51, color: Colors.pinkAccent),
+            Icon(Icons.accessibility, size: 51, color: Colors.indigo),
+
+            // font awesome icons:
+            FaIcon(FontAwesomeIcons.youtube, size: 51, color: Colors.red.shade700),
+            FaIcon(FontAwesomeIcons.amazon, size: 51, color: Colors.brown.shade700),
+            FaIcon(FontAwesomeIcons.google, size: 51, color: Colors.green.shade700),
+            FaIcon(FontAwesomeIcons.microsoft, size: 51, color: Colors.blueGrey.shade700),
+            FaIcon(FontAwesomeIcons.twitter, size: 51, color: Colors.blue.shade700),
+            FaIcon(FontAwesomeIcons.apple, size: 51, color: Colors.black),
+            FaIcon(FontAwesomeIcons.flutter, size: 51, color: Colors.blue.shade500),
+          ],
+        ),
       ),
     );
   }
